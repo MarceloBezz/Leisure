7161ae1763da2603783fe75b9a04a24f192a19ab
EnzoDevelopment <enzo.big.montano@gmail.com>
2023-11-03 13:06:06 -0300

    Version 9
    Insersão, atualização e visualização com SQL Server

--- leisure/src/main/java/org/deem/project/leisure/model/Usuario.java
+++ leisure/src/main/java/org/deem/project/leisure/model/Usuario.java
@@ -1,77 +1,142 @@
 package org.deem.project.leisure.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="Usuario")
+@Table(name="USUARIO")
 public class Usuario {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
 	private long id;
+	
+	@Column(name="NOME")
 	private String nome;
+	
+	@Column(name="EMAIL")
 	private String email;
-	private String telefone;
-	private String cpf;
-	private String endereco;
+	
+	@Column(name="SENHA")
 	private String senha;
+	
+	@Column(name="TELEFONE")
+	private String telefone;
+	
+	@Column(name="CPF")
+	private String cpf;
+	
+	@Column(name="CEP_ENDERECO")
+	private String cep;
+	
+	@Column(name="NUM_RESIDENCIA")
+	private short numResidencia;
+	
+	@Column(name="COMPLEMENTO")
+	private String complemento;
+	
+	@Column(name="ATIVO")
+	private boolean ativo = true;
 	
 	public Usuario() {
 	}
-	
-	public Usuario(long id, String nome, String email, String telefone, String cpf, String endereco, String senha) {
-		super();
+
+	public Usuario(long id, String nome, String email, String senha, String telefone, String cpf, String cep,
+			short numResidencia, String complemento, boolean ativo) {
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
+		this.senha = senha;
 		this.telefone = telefone;
 		this.cpf = cpf;
-		this.endereco = endereco;
-		this.senha = senha;
+		this.cep = cep;
+		this.numResidencia = numResidencia;
+		this.complemento = complemento;
+		this.ativo = ativo;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public short getNumResidencia() {
+		return numResidencia;
+	}
+
+	public void setNumResidencia(short numResidencia) {
+		this.numResidencia = numResidencia;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
 }
--- leisure/src/main/java/org/deem/project/leisure/repository/UsuarioRepository.java
+++ leisure/src/main/java/org/deem/project/leisure/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package org.deem.project.leisure.repository;
 
+import java.util.List;
+
 import org.deem.project.leisure.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	Usuario findById(long id);
+	List<Usuario> findAllByAtivo(boolean ativo);
+	List<Usuario> findByEmailOrCpf(String email, String cpf);
+	Usuario findByEmailAndSenha(String email, String senha);
 }

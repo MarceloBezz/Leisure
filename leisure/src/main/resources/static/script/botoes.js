cd5c73793e8e10a8742ae70e281db8c7a7710d2b
Danilo da Cruz <cruz.delfino@outlook.com>
2024-04-17 21:42:32 -0300

    13.10
    ajustes no front

--- leisure/src/main/resources/static/script/botoes.js
+++ leisure/src/main/resources/static/script/botoes.js
@@ -1,31 +1,65 @@
 //BOTOES FILTRO DA PAGINA DE PESQUISA
-
     const btnFiltro = document.querySelector('#btnFiltro')
-    const filtro = document.querySelector('.resultado-pesquisa__filtro')
+    const filtro = document.querySelector('.filtro__filtro')
     const menuLateral = document.querySelector('.lista-menu')
     const btnFiltroClose = document.querySelector('#btnFiltroClose')
     const btnHamburguer = document.querySelector('.container__rotulo i')
     const btnMenuLateralClose = document.querySelector('.home label')
 
-    btnFiltro.addEventListener('click', () => {
-        filtro.style.left = '0';
-        menuLateral.style.left = '-600px';
-    })
+//BOTAO PERFIL
+    const btnPerfilLista = document.querySelector('.logout__lista')
 
-    btnFiltroClose.addEventListener('click', () => {
-        filtro.style.left = '-600px';
-    })
+    try {
+        btnPerfil.addEventListener('click', function(event) {
+            event.stopPropagation();
+            btnPerfilLista.classList.toggle('hidden')
+        })
+        function closeClick(event) {
+            if(btnPerfilLista && ! btnPerfilLista.contains(event.target)) {
+                btnPerfilLista.classList.add('hidden');
+            }
+        }
+        
+        document.addEventListener('click', closeClick)
+    } catch (error) {
+        
+    }
+//FIM
 
-    btnHamburguer.addEventListener('click', () => {
-        menuLateral.style.left < '0'
-        ? menuLateral.style.left = '0' || btnMenuIcone.classList.remove('fa-bars') || btnMenuIcone.classList.add('fa-xmark') 
-        : menuLateral.style.left = '-600px' || btnMenuIcone.classList.remove('fa-xmark') || btnMenuIcone.classList.add('fa-bars');
-    })
+    try {
+        btnFiltro.addEventListener('click', () => {
+            filtro.style.left = '0';
+            menuLateral.style.left = '-600px';
+        })
+    }catch (error) {
 
-    btnMenuLateralClose.addEventListener('click', () => {
-        menuLateral.style.left = '-600px';
-        btnMenuIcone.classList.remove('fa-xmark');
-        btnMenuIcone.classList.add('fa-bars');
-    })
+    }
+
+    try {
+        btnFiltroClose.addEventListener('click', () => {
+            filtro.style.left = '-600px';
+        }) 
+    }catch (error) {
+
+    }
     
+    try {
+        btnHamburguer.addEventListener('click', () => {
+            menuLateral.style.left < '0'
+            ? menuLateral.style.left = '0' || btnMenuIcone.classList.remove('fa-bars') || btnMenuIcone.classList.add('fa-xmark') 
+            : menuLateral.style.left = '-600px' || btnMenuIcone.classList.remove('fa-xmark') || btnMenuIcone.classList.add('fa-bars');
+        })
+    }catch (error) {
+
+    }
+
+    try {
+        btnMenuLateralClose.addEventListener('click', () => {
+            menuLateral.style.left = '-600px';
+            btnMenuIcone.classList.remove('fa-xmark');
+            btnMenuIcone.classList.add('fa-bars');
+        })
+    }catch (error) {
+
+    }
 //FIM

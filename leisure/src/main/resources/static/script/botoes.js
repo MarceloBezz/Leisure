20b5385aa1c88f49d4e912ba58eaea9241f3783e
Danilo da Cruz <cruz.delfino@outlook.com>
2024-04-27 21:23:51 -0300

    ajustes no JS, ajustando layout da pagina de pesquisa.

--- leisure/src/main/resources/static/script/botoes.js
+++ leisure/src/main/resources/static/script/botoes.js
@@ -1,10 +1,27 @@
 //BOTOES FILTRO DA PAGINA DE PESQUISA
-    const btnFiltro = document.querySelector('#btnFiltro')
+    const btnFiltro = document.querySelectorAll('[data-filtro]');
+
     const filtro = document.querySelector('.filtro__filtro')
     const menuLateral = document.querySelector('.lista-menu')
-    const btnFiltroClose = document.querySelector('#btnFiltroClose')
     const btnHamburguer = document.querySelector('.container__rotulo i')
     const btnMenuLateralClose = document.querySelector('.home label')
+
+    const btnSubmenu = document.querySelectorAll('[data-menu]');
+    const btnSubmenuLista = document.querySelectorAll('[data-submenu]');
+    const btnArrow = document.querySelectorAll('[data-arrow]');
+
+//BOTAO CATEGORIAS E NOSSO APP
+    try{
+        btnSubmenu.forEach((botao, index) => {
+            botao.addEventListener('click', () => {
+                btnSubmenuLista[index].classList.toggle('hidden');
+                btnArrow[index].classList.toggle('fa-chevron-right');
+                btnArrow[index].classList.toggle('fa-chevron-down');
+            })
+        })
+    }catch {
+
+    }
 
 //BOTAO PERFIL
     const btnPerfilLista = document.querySelector('.logout__lista')
@@ -15,41 +32,33 @@
             btnPerfilLista.classList.toggle('hidden')
         })
         function closeClick(event) {
-            if(btnPerfilLista && ! btnPerfilLista.contains(event.target)) {
-                btnPerfilLista.classList.add('hidden');
-            }
+            // if(btnPerfilLista && ! btnPerfilLista.contains(event.target)) {
+            //     btnPerfilLista.classList.add('hidden');
+            // }
+            btnPerfilLista && ! btnPerfilLista.contains(event.target) ? btnPerfilLista.classList.add('hidden') : null;
         }
         
         document.addEventListener('click', closeClick)
-    } catch (error) {
+    } catch {
         
     }
 //FIM
 
-    try {
-        btnFiltro.addEventListener('click', () => {
-            filtro.style.left = '0';
-            menuLateral.style.left = '-600px';
+    btnFiltro.forEach(botao => {
+        botao.addEventListener('click', () => {
+            const btnFiltroAtributo = botao.getAttribute('data-filtro');
+            btnFiltroAtributo === "filtrar" ? (filtro.style.left = '0', menuLateral.style.left = '-600px') : null;
+            btnFiltroAtributo === "filtrarClose" ? filtro.style.left = '-600px' : null;
         })
-    }catch (error) {
-
-    }
-
-    try {
-        btnFiltroClose.addEventListener('click', () => {
-            filtro.style.left = '-600px';
-        }) 
-    }catch (error) {
-
-    }
+    });
     
     try {
         btnHamburguer.addEventListener('click', () => {
             menuLateral.style.left < '0'
-            ? menuLateral.style.left = '0' || btnMenuIcone.classList.remove('fa-bars') || btnMenuIcone.classList.add('fa-xmark') 
-            : menuLateral.style.left = '-600px' || btnMenuIcone.classList.remove('fa-xmark') || btnMenuIcone.classList.add('fa-bars');
+            ? (menuLateral.style.left = '0', btnMenuIcone.classList.remove('fa-bars'), btnMenuIcone.classList.add('fa-xmark')) 
+            : (menuLateral.style.left = '-600px', btnMenuIcone.classList.remove('fa-xmark'), btnMenuIcone.classList.add('fa-bars'));
         })
-    }catch (error) {
+    }catch {
 
     }
 
@@ -59,7 +68,7 @@
             btnMenuIcone.classList.remove('fa-xmark');
             btnMenuIcone.classList.add('fa-bars');
         })
-    }catch (error) {
+    }catch {
 
     }
 //FIM

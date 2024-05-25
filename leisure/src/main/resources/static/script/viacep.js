d7084454e6554fa0af395bf9a9896160bd19afa5
Danilo da Cruz <cruz.delfino@outlook.com>
2024-05-25 17:15:37 -0300

    Ajustes no Anuncio - Adicionado Via cep no cadastro de imoveis

--- leisure/src/main/resources/static/script/viacep.js
+++ leisure/src/main/resources/static/script/viacep.js
@@ -1,67 +1,42 @@
 function limpa_formulário_cep() {
-        //Limpa valores do formulário de cep.
-        document.getElementById('rua').value=("");
-        document.getElementById('bairro').value=("");
-        document.getElementById('cidade').value=("");
-        document.getElementById('uf').value=("");
-        // document.getElementById('ibge').value=("");
+    document.getElementById('rua').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    // document.getElementById('estado').value = "";
 }
 
 function meu_callback(conteudo) {
     if (!("erro" in conteudo)) {
-        //Atualiza os campos com os valores.
-        document.getElementById('rua').value=(conteudo.logradouro);
-        document.getElementById('bairro').value=(conteudo.bairro);
-        document.getElementById('cidade').value=(conteudo.localidade);
-        document.getElementById('uf').value=(conteudo.uf);
-        // document.getElementById('ibge').value=(conteudo.ibge);
-    } //end if.
-    else {
-        //CEP não Encontrado.
+        document.getElementById('rua').value = conteudo.logradouro;
+        document.getElementById('bairro').value = conteudo.bairro;
+        document.getElementById('cidade').value = conteudo.localidade;
+        // document.getElementById('estado').value = conteudo.uf;
+    } else {
         limpa_formulário_cep();
         alert("CEP não encontrado.");
     }
 }
-    
-function pesquisacep(valor) {
 
-    //Nova variável "cep" somente com dígitos.
+function pesquisacep(valor) {
     var cep = valor.replace(/\D/g, '');
 
-    //Verifica se campo cep possui valor informado.
     if (cep != "") {
-
-        //Expressão regular para validar o CEP.
         var validacep = /^[0-9]{8}$/;
 
-        //Valida o formato do CEP.
         if(validacep.test(cep)) {
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            // document.getElementById('estado').value = "...";
 
-            //Preenche os campos com "..." enquanto consulta webservice.
-            document.getElementById('rua').value="...";
-            document.getElementById('bairro').value="...";
-            document.getElementById('cidade').value="...";
-            document.getElementById('uf').value="...";
-            // document.getElementById('ibge').value="...";
-
-            //Cria um elemento javascript.
             var script = document.createElement('script');
-
-            //Sincroniza com o callback.
-            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-            //Insere script no documento e carrega o conteúdo.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
             document.body.appendChild(script);
-
-        } //end if.
-        else {
-            //cep é inválido.
+        } else {
             limpa_formulário_cep();
             alert("Formato de CEP inválido.");
         }
-    } //end if.
-    else {
-        //cep sem valor, limpa formulário.
+    } else {
         limpa_formulário_cep();
     }
-};
+}

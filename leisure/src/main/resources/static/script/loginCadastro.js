5e37134c62ff51c99fa38d177e0556c300c96501
Danilo da Cruz <cruz.delfino@outlook.com>
2024-06-05 17:42:11 -0300

    Ajustes na loginCadastro -ajustado para melhor experiencia do usuario

---
+++ leisure/src/main/resources/static/script/loginCadastro.js
@@ -0,0 +1,14 @@
+const btnCadastro = document.querySelector('[data-acesso="btn-cadastro"]')
+const btnLogin = document.querySelector('[data-acesso="btn-login"]')
+const campoLogin = document.querySelector('[data-acesso="login"]')
+const campoCadastro = document.querySelector('[data-acesso="cadastro"]')
+
+btnCadastro.addEventListener('click', () => {
+    campoLogin.style.display = 'none'
+    campoCadastro.style.display = 'block'
+})
+
+btnLogin.addEventListener('click', () => {
+    campoCadastro.style.display = 'none'
+    campoLogin.style.display = 'block'
+})

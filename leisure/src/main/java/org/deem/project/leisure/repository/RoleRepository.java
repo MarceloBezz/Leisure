aa27b2fb0d11b024d591e849a3b331cab1338f2e
Marcelo <mdbezerra16@gmail.com>
2024-06-16 12:03:04 -0300

    Ajuste no roleRepository

--- leisure/src/main/java/org/deem/project/leisure/repository/RoleRepository.java
+++ leisure/src/main/java/org/deem/project/leisure/repository/RoleRepository.java
@@ -8,12 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+
 public interface RoleRepository extends JpaRepository<Roles, Long>{
 	
-	@Query(value="SELECT * FROM perfil p WHERE p.tipo=?", nativeQuery=true)
-	Roles findByName(String roles);
+	@Query(value="SELECT * FROM perfil p WHERE p.tipo=tipo", nativeQuery=true)
+	Roles findByName(@Param("tipo")String roles);
 
-	@Query(value="SELECT id_perfil FROM usuario_perfil WHERE ID_USUARIO =?", nativeQuery = true)
-	List<Long> findPapeisById(Long id);
+	// @Query(value="SELECT id_perfil FROM usuario_perfil WHERE ID_USUARIO =?", nativeQuery = true)
+	// List<Long> findPapeisById(Long id);
 
 }
